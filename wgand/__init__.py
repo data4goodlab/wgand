22ee3c2461cea2ad9631056ef9f0c73070c7e7f5
Dim Kagan <dimakagan15@gmail.com>
2023-04-09 12:42:11 +0300

    Add PcaAnomalyDetector to imports

--- wgand/__init__.py
+++ wgand/__init__.py
@@ -1,4 +1,5 @@
 from wgand.node_classifier import NodeClassifier
 from wgand.anomaly_detector import AnomalyDetector
+from wgand.pca_anomaly_detector import PcaAnomalyDetector
 from wgand.utils import load_tissue_graph, get_gdf
 
