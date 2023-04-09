8c6c2617b6002aa654f46d8c18bc5888193cc042
Dim Kagan <dimakagan15@gmail.com>
2023-04-09 12:43:36 +0300

    Remove pca related functionallity and API update

--- wgand/anomaly_detector.py
+++ wgand/anomaly_detector.py
@@ -1,5 +1,6 @@
 import numpy as np
-from wgand.base_estimator import BaseEstimator
+from wgand.base_detector import BaseDetector
+from wgand.pca_anomaly_detector import PcaAnomalyDetector
 from wgand.utils import sigmoid
 from sklearn.decomposition import PCA
 from sklearn.utils.validation import check_is_fitted
@@ -8,14 +9,14 @@ from sklearn.preprocessing import MinMaxScaler
 import pyod.models.combination as comb 
 
 
-class AnomalyDetector(BaseEstimator):
+class AnomalyDetector(PcaAnomalyDetector):
     """
     Node outlier detector
     """
     
     def __init__(self, g, weight_clf, meta_clf, embedding_model=None, feature_selection=False, n_components=0):
         super(AnomalyDetector, self).__init__(g, weight_clf, meta_clf, embedding_model, feature_selection, n_components)
-        self.pca_tran = PCA(n_components=2, random_state=2)
+        # self.pca_tran = PCA(n_components=2, random_state=2)
         
     def fit(self, X):
         """
@@ -25,33 +26,24 @@ class AnomalyDetector(BaseEstimator):
         X : list
             List of nodes to score on
         """
-        try:
-            check_is_fitted(self.weight_clf)
-        except NotFittedError:
-            self.train_weight_classifier()
+        super().fit(X)
             
-        probs2 = self.predict_feature_score(X)
+        probs_features = self.predict_feature_score(X)
 
         X = self.get_node_training_data(X)
 
 
-        self.pca_tran.fit(X)
+        probs_pca = super().predict_proba(X, False) 
 
         X_pca = self.pca_tran.transform(X)
-    
-        scaler = MinMaxScaler().fit(X_pca[:,1:])
-        probs1 = scaler.transform(X_pca[:,1:]).ravel().clip(0, 1).reshape(-1, 1)
-        probs1 = sigmoid(X_pca[:,1]).reshape(-1, 1) 
 
-        
-
-        X = np.concatenate([X, X_pca, probs1, probs2], axis=1)
+        X = np.concatenate([X, X_pca, probs_pca, probs_features], axis=1)
         self.node_clf.fit(X)
-
     
-    def predict_feature_score(self, nodes):
+
+    def predict_proba(self, nodes):
         """
-        Predict the feature score for a list of nodes
+        Predict the anomaly score for a list of nodes
         Parameters
         ----------
         nodes : list
@@ -61,12 +53,21 @@ class AnomalyDetector(BaseEstimator):
         scores : np.array
             Array of scores for the nodes
         """
-        check_is_fitted(self.weight_clf)
-        X = self.get_node_training_data(nodes)
-
-        scores = sigmoid(X)
-
-        return comb.average(scores).reshape(-1, 1)
+        return self.predict_node_proba(nodes)
+    
+    def predict(self, nodes):
+        """
+        Predict the anomaly score for a list of nodes
+        Parameters
+        ----------
+        nodes : list
+            List of nodes to score
+        Returns
+        -------
+        scores : np.array
+            Array of scores for the nodes
+        """
+        return self.predict_node_proba(nodes)[:,1]>0.5
 
     def predict_node_proba(self, nodes):
         """
@@ -80,23 +81,25 @@ class AnomalyDetector(BaseEstimator):
         scores : np.array
             Array of scores for the nodes
         """
-        check_is_fitted(self.node_clf)
+        self.check_is_fitted()
         X = self.get_node_training_data(nodes)
 
         X_pca = self.pca_tran.transform(X)
-    
-        scaler = MinMaxScaler().fit(X_pca[:,1:])
-        probs1 = scaler.transform(X_pca[:,1:]).ravel().clip(0, 1).reshape(-1, 1)
-        probs1 = sigmoid(X_pca[:,1]).reshape(-1, 1) 
+        probs_proba = sigmoid(X_pca[:,1]).reshape(-1, 1) 
 
-        probs2 = self.predict_feature_score(nodes)
+        probs_features = self.predict_feature_score(nodes)
 
-        X = np.concatenate([X, X_pca, probs1, probs2], axis=1)
+        X = np.concatenate([X, X_pca, probs_proba, probs_features], axis=1)
         scores = [self.node_clf.predict_proba(X)[:,1:]]
-        scores.append(probs1)
+        scores.append(probs_proba)
 
-        scores.append(probs2)
+        scores.append(probs_features)
 
         scores = np.concatenate(scores, axis=1)
 
         return comb.maximization(scores)
+
+
+    def check_is_fitted(self):
+        check_is_fitted(self.node_clf)
+        return super().check_is_fitted()
