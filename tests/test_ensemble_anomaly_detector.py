1475076034db289a1e8ffddc728ac662af20f0d2
Dim Kagan <dimakagan15@gmail.com>
2023-04-18 10:04:01 +0300

    Add first basic test

---
+++ tests/test_ensemble_anomaly_detector.py
@@ -0,0 +1,35 @@
+from pyod.models.iforest import IForest
+from sklearn.ensemble import RandomForestRegressor
+from wgand import EnsembleAnomalyDetector
+from wgand.utils import load_tissue_graph, get_metrics
+from karateclub.node_embedding.neighbourhood import RandNE
+from wgand.utils import get_metrics, get_disease_info_df, get_tissue_mapping_df
+import networkx as nx
+import numpy as np
+
+'''
+Functions to test the EnsembleAnomalyDetector class.
+'''
+def generate_random_weighted_graph():
+    """
+    Generate a random weighted graph.
+    """
+    G = nx.erdos_renyi_graph(100, 0.1)
+    for edge in G.edges():
+        G.edges[edge]["weight"] = np.random.uniform(0, 1)
+    return G
+
+def test_ensemble_anomaly_detector():
+    """
+    Test the EnsembleAnomalyDetector class.
+    """
+    G = generate_random_weighted_graph()
+    model = EnsembleAnomalyDetector(
+        base_estimator=IForest(),
+        ensemble_estimator=RandomForestRegressor(),
+        embedding_estimator=RandNE(),
+    )
+    model.fit(G)
+    scores = model.predict(list(G.nodes()))
+    assert scores.shape[0] == G.number_of_nodes()
+    
